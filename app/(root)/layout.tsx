b0b275e0c61a1238be9ac930e3611e3b33509f6c
kariimghariib <karimgharib272@gmail.com>
2025-04-17 18:52:05 +0200

    sign in and sign up functionality

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,28 @@
+import { Button } from "@/components/ui/button";
+import { isAuthenticated, signOut } from "@/lib/actions/auth.action";
+import Image from "next/image";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 import React, { ReactNode } from "react";
 
-const RootLayout = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+const RootLayout = async ({ children }: { children: ReactNode }) => {
+  const isUserAuthenticated = await isAuthenticated();
+  if (!isUserAuthenticated) redirect("/sign-in");
+
+  return (
+    <div className="root-layout">
+      <nav className="flex items-center justify-between">
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="logo" width={38} height={32} />
+          <h2 className="text-primary-100">PrepWise</h2>
+        </Link>
+        {/* <Button className="btn" onClick={signOut}>
+          Sign out
+        </Button> */}
+      </nav>
+      <div>{children}</div>
+    </div>
+  );
 };
 
 export default RootLayout;
