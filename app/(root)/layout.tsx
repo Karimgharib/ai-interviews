fa85acef03f2172d45e133d0a78ab773f40dc1b6
kariimghariib <karimgharib272@gmail.com>
2025-05-04 01:46:06 +0300

    implement interview generation api

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -14,7 +14,7 @@ const RootLayout = async ({ children }: { children: ReactNode }) => {
       <nav className="flex items-center justify-between">
         <Link href="/" className="flex items-center gap-2">
           <Image src="/logo.svg" alt="logo" width={38} height={32} />
-          <h2 className="text-primary-100">PrepWise</h2>
+          <h2 className="text-primary-100">TechInterviews</h2>
         </Link>
         <Button className="btn-primary" onClick={signOut}>
           Sign out
--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const monaSans = Mona_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "PrepWise",
+  title: "TechInterviews",
   description: "AI platform for preparing for interviews",
 };
 
